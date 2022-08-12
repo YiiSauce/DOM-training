3c7ea8d9da616239b7096a527cba96ef869c8bd1
YiiSauce <youssefyouss321@gmail.com>
2022-08-12 13:12:07 +0100

    Thired

--- Web/app.js
+++ Web/app.js
@@ -5,6 +5,14 @@ let max = kalam.maxLength;
 function func(event){
     let remain = max - event.target.value.length;
     combien.textContent = remain;
+    if(remain<10){
+        combien.classList.add("warning");
+        kalam.classList.add("warning");
+    }
+    else{
+        combien.classList.remove("warning");
+        kalam.classList.remove("warning");
+    }
 }
 
 kalam.addEventListener("input", func)
